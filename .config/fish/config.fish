4a10e071c7fdb6c9521cea943c85f0f722c89d25
Slayug <bug.graph@gmail.com>
2019-08-08 15:55:28 +0200

    load notif function, add local npm package path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,12 +1,25 @@
-
-
-function start_tmux
-    if type tmux > /dev/null
-        #if not inside a tmux session, and if no session is started, start a new session
-        if test -z "$TMUX" ; and test -z $TERMINAL_CONTEXT
-            tmux -2 attach; or tmux -2 new-session
-        end
-    end
-end
-
-start_tmux
+function start_tmux                                                                                                                                                                           
+     if type tmux > /dev/null                                                                                                                                                                  
+         #if not inside a tmux session, and if no session is started, start a new session                                                                                                      
+         if test -z "$TMUX" ; and test -z $TERMINAL_CONTEXT                                                                                                                                    
+             tmux -2 attach; or tmux -2 new-session                                                                                                                                            
+         end                                                                                                                                                                                   
+     end                                                                                                                                                                                       
+ end                                                                                                                                                                                           
+                                                                                                                                                                                               
+ # npm                                                                                                                                                                                         
+ set NPM_PACKAGES "$HOME/.npm-packages"                                                                                                                                                        
+ set PATH $NPM_PACKAGES/bin $PATH                                                                                                                                                              
+                                                                                                                                                                                               
+ # Unset manpath so we can inherit from /etc/manpath via the `manpath` command                                                                                                                 
+ set -e MANPATH                                                                                                                                                                                
+ set MANPATH $NPM_PACKAGES/share/man $MANPATH                                                                                                                                                  
+                                                                                                                                                                                               
+ alias kub="kubectl"                                                                                                                                                                           
+                                                                                                                                                                                               
+ start_tmux                                                                                                                                                                                    
+                                                                                                                                                                                               
+ # The next line updates PATH for the Google Cloud SDK.                                                                                                                                        
+ if [ -f '/home/bug/Documents/soft/google-cloud-sdk/path.fish.inc' ]; . '/home/bug/Documents/soft/google-cloud-sdk/path.fish.inc'; end                                                         
+                                                                                                                                                                                               
+ . ~/.config/fish/functions/notif.fish
